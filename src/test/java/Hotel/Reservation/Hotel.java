97451418c6955d3b25ff118b67ce7eefa99936fe
swapnaneel bhuiya <bhuiya.swapnaneel@gmail.com>
2020-10-05 16:18:53 +0530

    [swapnaneel]Initial

---
+++ src/test/java/Hotel/Reservation/Hotel.java
@@ -0,0 +1,56 @@
+package Hotel.Reservation;
+
+public class Hotel {
+	private String hotel_name;
+	private int rating;
+	private int reg_cost;
+	private int reg_weekend;
+	private int rew_cost;
+	private int rew_weekend;
+	public Hotel(String ar, int a, int b, int c, int d, int e)
+	{
+		hotel_name=ar;
+		rating=a;
+		reg_cost=b;
+		reg_weekend=c;
+		rew_cost=d;
+		rew_weekend=e;
+	}
+	public String getHotel_name() {
+		return hotel_name;
+	}
+	public void setHotel_name(String hotel_name) {
+		this.hotel_name = hotel_name;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public int getReg_cost() {
+		return reg_cost;
+	}
+	public void setReg_cost(int reg_cost) {
+		this.reg_cost = reg_cost;
+	}
+	public int getReg_weekend() {
+		return reg_weekend;
+	}
+	public void setReg_weekend(int reg_weekend) {
+		this.reg_weekend = reg_weekend;
+	}
+	public int getRew_cost() {
+		return rew_cost;
+	}
+	public void setRew_cost(int rew_cost) {
+		this.rew_cost = rew_cost;
+	}
+	public int getRew_weekend() {
+		return rew_weekend;
+	}
+	public void setRew_weekend(int rew_weekend) {
+		this.rew_weekend = rew_weekend;
+	}
+
+}
---
+++ src/test/java/Hotel/Reservation/HotelResMain.java
@@ -0,0 +1,12 @@
+package Hotel.Reservation;
+
+public class HotelResMain {
+	
+	public static Hotel obj1=new Hotel("Lakewood",3,110,90,80,80);
+	public static Hotel obj2=new Hotel("Bridgewood",4,160,60,110,50);
+	public static Hotel obj3=new Hotel("Ridgewood",5,220,150,100,40);
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to Hotel Reservation Program");
+	}
+}
